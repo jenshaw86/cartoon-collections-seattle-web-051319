2b0d8542a61154ad20e0ec949421e6e5209adcf3
Jenny Shaw <bloo.j.shaw@gmail.com>
2019-05-06 13:27:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,13 +21,14 @@ def find_the_cheese(items) # arg contains list of items called "items"
   # array "cheese_types" to compare against arg "items"
   cheese_types = ["cheddar", "gouda", "camembert"]
   
-  # use find to find first item that is included in cheese list
+  # use .find to find first item that is included in cheese list
   items.find {|item|
     cheese_types.include?(item)
   }
 end
 
-# Using any instead of include. 
+# Using any instead of include.
+# Use .find to find first item that matches .any? cheese
 
 # def find_the_cheese(items) # arg contains list of items called "items"
 #   # array "cheese_types" to compare against arg "items"
